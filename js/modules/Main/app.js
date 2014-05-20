75583d6f98f2d76266038f4ad990aca4b899d04b
brian978 <andrei.dincescu@hotmail.com>
2014-05-20 16:40:19 +0300

    Migrated the demo page to the new format

--- js/modules/Main/app.js
+++ js/modules/Main/app.js
@@ -6,21 +6,21 @@
  * @license Creative Commons Attribution-ShareAlike 3.0
  */
 
-$(function () {
-    var sketcher = new EasySketch.Sketch("#drawing-canvas", {doubleBuffering: true});
+requirejs(["EasySketch/Sketch"], function(Sketch){
+    var sketcher = new Sketch("#drawing-canvas", {doubleBuffering: true});
 
     // Disables the eraser
-    $('#pencil-btn').on('click', function () {
+    $('#pencil').on('click', function () {
         sketcher.enableEraser(false);
     });
 
     // Enables the eraser
-    $('#eraser-btn').on('click', function () {
+    $('#eraser').on('click', function () {
         sketcher.enableEraser(true);
     });
 
     // Enables the eraser
-    $('#clear-btn').on('click', function () {
+    $('#clear').on('click', function () {
         sketcher.clear();
     });
 
@@ -56,38 +56,38 @@ $(function () {
     // Predefined line
     sketcher.setOptions({alpha: 0.1});
     sketcher.drawLine([
-                          {
-                              x: 20,
-                              y: 10
-                          },
-                          {
-                              x: 40,
-                              y: 100
-                          },
-                          {
-                              x: 60,
-                              y: 10
-                          }
-                      ]);
+        {
+            x: 20,
+            y: 10
+        },
+        {
+            x: 40,
+            y: 100
+        },
+        {
+            x: 60,
+            y: 10
+        }
+    ]);
     sketcher.drawLine([
-                          {
-                              x: 5,
-                              y: 10
-                          },
-                          {
-                              x: 15,
-                              y: 50
-                          },
-                          {
-                              x: 30,
-                              y: 10
-                          }
-                      ]);
+        {
+            x: 5,
+            y: 10
+        },
+        {
+            x: 15,
+            y: 50
+        },
+        {
+            x: 30,
+            y: 10
+        }
+    ]);
     sketcher.setOptions({alpha: 1});
 
     // An event that is triggered when the user draws on the canvas
     // (this does not trigger when the DRAW_EVENT is called via the event manager)
-    sketcher.getEventManager().attach(EasySketch.Sketch.NOTIFY_DRAW_EVENT, function (e, mouse) {
-        console.log('drawing at ' + JSON.stringify(mouse));
+    sketcher.getEventManager().attach(Sketch.NOTIFY_PAINT_EVENT, function (event) {
+        console.log('drawing at ' + JSON.stringify(event.getParam(0)));
     });
 });
