81d88fdd8cfe42ca86ac2c3f78e24812b9574cdf
brian978 <andrei.dincescu1@gmail.com>
2015-04-22 09:06:54 +0300

    Updated build Updated copyright Removed some file headers

--- src/js/modules/EasySketch/EasySketch.js
+++ src/js/modules/EasySketch/EasySketch.js
@@ -2,7 +2,7 @@
  * easy-sketch.js
  *
  * @link https://github.com/brian978/easy-sketch.js
- * @copyright Copyright (c) 2014
+ * @copyright Copyright (c) 2015
  * @license https://github.com/brian978/easy-sketch.js/blob/master/LICENSE New BSD License
  */
 
